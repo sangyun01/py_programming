858e8a423b01456e339f9e4b8537188f86b26518
sangyun01 <worldtop0612@gmail.com>
2024-09-05 15:46:03 +0900

    first week

---
+++ #first week.py
@@ -0,0 +1,33 @@
+#first week
+
+'''
+#print("Hello world")
+
+#print(3.14*10*10)
+
+#print(3.14*10**2)
+
+#print("나의" + "고양이")
+
+print("hi"*10)
+
+print('"안녕하세요"')
+
+print('"programming에 입문하신 것을 축하드립니다."')
+
+print("생일축하!!"*10)
+
+#문자열이라서 ""안에 출력해야함
+
+#print(f"결과값은 {10}입니다.")
+
+print(f"3 * 1 = {3*1}")
+print(f"3 * 2 = {3*2}")
+print(f"3 * 3 = {3*3}")
+'''
+
+for i in [1,2,3] :
+    print(f"3 * {i} = {3*i}")
+    
+    
+    
