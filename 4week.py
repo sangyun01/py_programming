2d8de1b21bdb898e10eb1fe16f4c8e5848905a5f
sangyun01 <worldtop0612@gmail.com>
2024-09-19 16:46:44 +0900

    HW3 초안

--- 4week.py
+++ 4week.py
@@ -1,1 +1,13 @@
-#4주차 반복문
+# 4주차 반복문
+
+# Lab: 팩토리얼 계산하기
+num = int(input("정수를 입력하시오: "))
+
+
+# Lab: 구구단 출력
+dan = int(input("원하는 단은: "))
+i = 1
+
+while i <= 9:
+    print(f"{dan}X{i} = {dan*i}")
+    i += 1
