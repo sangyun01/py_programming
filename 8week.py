92a82d92fac241e1c3e028cb3c3bc2243859c978
sangyun01 <worldtop0612@gmail.com>
2024-10-16 01:58:54 +0900

    잘못건듬 ㅎ

--- 8week.py
+++ 8week.py
@@ -1,5 +1,7 @@
 # 8주차 객체와 클래스
 
+# Lab: TV 클래스 정의
+
 class TVsettings:
     def __init__(self, channel, volume, ON):
         self.setChannel = channel
@@ -9,5 +11,8 @@ class TVsettings:
     def show(self):
         print(f"{self.setChannel} {self.getvolume} {self.turnon}")
 
+
 samsung = TVsettings(11, 20, True)
 samsung.show()
+
+#dkdkdkdk
