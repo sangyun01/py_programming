7b61a0577343d6dcbb51b906c99167a7970f251f
sangyun01 <worldtop0612@gmail.com>
2024-11-12 23:07:31 +0900

    p.25 done

--- 11week.py
+++ 11week.py
@@ -1,5 +1,6 @@
 # 11주차 내장 함수
 
+"""
 # Lab : 내장 함수 예제
 invitations = ["Kim", "Lee", "Park", "Choi"]
 persons = [1, 3, 0, 6]
@@ -16,5 +17,26 @@ else:
     print("모든 그룹이 오지는 않습니다.")
 
 print(f"{max(persons)}")
+"""
 
 # Lab : 키를 이용한 정렬 예제
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return f"<이름 : {self.name}, 나이 : {self.age}>"
+
+people = [Person("홍길동", 20), Person("최자영", 38), Person("김철수", 35)]
+
+
+def keyage(person):
+    return person.age
+"""
+def keyname(person):
+    return person.name
+
+print(sorted(people, key=keyname)) #정렬기준이 key=keyname(이름순)
+"""
+print(sorted(people, key=keyage)) #정렬기준이 keyage
