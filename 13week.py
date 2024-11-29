3b819d1ea27054395abc9c027e3569c79fb98244
Yuni_PC <worldtop0612@gmail.com>
2024-11-29 16:51:36 +0900

    123

--- 13week.py
+++ 13week.py
@@ -1,2 +1,13 @@
-#14장 넘파이
+# 14장 넘파이
 
+import numpy as np
+
+heights = [1.83, 1.76, 1.69, 1.86, 1.77, 1.73]
+weights = [86, 74, 59, 95, 80, 68]
+
+heights = np.array(heights)
+weights = np.array(weights)
+
+bmi = weights / (heights**2)
+
+print(bmi)
