56c5019cba2a51b33c13f96b51e030b680a20ba0
sangyun01 <worldtop0612@gmail.com>
2024-12-03 15:25:41 +0900

    numpy done

--- 13week.py
+++ 13week.py
@@ -62,10 +62,33 @@ y=np.array([0,1,0,0,0])
 MSE = np.mean((ypred-y)**2)
 
 print(MSE)
-"""
 
 #Lab: 직원들의 월급 인상하기
+import numpy as np
+
+current_salary = np.array([220, 250, 330])
+ot1_current_salary = np.array([100,200,300])
+ot2_current_salary = np.array([100,300,500])
+
+current_salary += 100
+ot1_current_salary *=2
+ot2_current_people = ot2_current_salary[ot2_current_salary>450]
+
+print(f"100만원 인상된 월급 :{current_salary}")
+print(f"2배 인상된 월급 : {ot1_current_salary}")
+print(f"450만윈 이상인 직원의 월급 : {ot2_current_people}")
 
 #Lab: 그래프 그리기
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(1,10)
+
+y0 = [1]*50
+y1 = x
+y2 = x**2
 
 
+plt.plot(x,y0,x,y1,x,y2)
+plt.show()
+"""
